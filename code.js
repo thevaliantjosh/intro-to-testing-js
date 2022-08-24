14d512e862ece93161cbe21d64acc4d280852c64
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-08-24 12:27:23 -0500

    First Test of Hello World

--- code.js
+++ code.js
@@ -1,4 +1,29 @@
-// helloWorld function 
+// helloWorld function
+// function helloWorld() {
+//     return "Hello";
+// }
+//
+// function hello() {
+//     return "Hello, World!";
+// }
+//
+// function helloWorld() {
+//     console.log( "Hello, World!");
+//
+// function helloWorld() {
+//     return "Hello, World!;
+// }
+
+//Function Statement
+// function helloWorld() {
+//     return "Hello, World!";
+// }
+
+//function expression
+// const helloWorld = function() {
+//     return "Hello, World!";
+// }
+
 function helloWorld() {
-    return "Hello, World!";
+
 }
--- tests.js
+++ tests.js
@@ -1,15 +1,34 @@
 // Unit tests for the helloWorld function
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
-    });
-    it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
-    });
-    it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
-    });
-    it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+
+// Step 1: Let's write the smallest test possible. Open up tests.js. Add a describe, an it, and an expect to assert that sayHello is a defined function. Use your tests for helloWorld as a guide.
+// Step 2: Run all the tests. At this point, we're expecting and hoping for a single, red failing test that we just now authored.
+// Step 3: Now, let's go to code.js and create an empty function definition for sayHello.
+// Step 4: Run all the tests. We're expecting all tests, including the new test for sayHello to be green.
+// Step 5: Given that this is our first (tiny) test and our first implementation, there is not yet the opportunity to refactor.
+//     Step 6: The last TDD step is to "repeat" the process of adding another test. What we're going to do is add our work to git and then move to Exercise #2, which is to add the second test.
+
+
+describe("helloWorld", function() {
+    it('should be a defined function', function () {
+        expect(typeof helloWorld).toBe("function");
     });
 });
+
+
+
+
+
+// describe('helloWorld', function() {
+//     it('should be a defined function', function() {
+//       expect(typeof helloWorld).toBe('function');
+//     });
+//     it('should return a string when called', function() {
+//       expect(typeof helloWorld()).toBe("string");
+//     });
+//     it('should return the string "Hello, World!" when executed', function() {
+//       expect(helloWorld()).toBe("Hello, World!");
+//     });
+//     it("should never return 'undefined' when called", function() {
+//       expect(helloWorld()).not.toBe(undefined);
+//     });
+// });
