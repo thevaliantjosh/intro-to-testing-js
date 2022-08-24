afb202e98d41737af44e21bd5480a2c0168c5bec
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-08-24 16:17:40 -0500

    More Done

--- tests.js
+++ tests.js
@@ -15,13 +15,13 @@ describe("sayHello", function() {
         expect(typeof sayHello()).toBe("string")
     });
     it('should return the string "Hello, Jane!" when executed', function () {
-        expect(sayHello()).toBe("Hello, Jane!");
+        expect(sayHello("Jane")).toBe("Hello, Jane!");
     });
     it(`should return the string "Hello, Alex!" when executed`, function () {
-        expect(sayHello("Alex!")).toBe("Hello, Alex!");
+        expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
     it('should return the string "Hello, Pat!" when executed', function () {
-        expect(sayHello("Pat!")).toBe("Hello, Pat!");
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
     });
 });
 
