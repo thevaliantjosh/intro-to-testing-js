3bb94ec19ee359c396f08400812f405a089f439e
Joshua Sprouse <joshuacalebsprouse@gmail.com>
2022-08-24 13:30:55 -0500

    Finished Exercise 2

--- code.js
+++ code.js
@@ -25,5 +25,5 @@
 // }
 
 function helloWorld() {
-
+    return "";
 }
--- tests.js
+++ tests.js
@@ -7,10 +7,22 @@
 // Step 5: Given that this is our first (tiny) test and our first implementation, there is not yet the opportunity to refactor.
 //     Step 6: The last TDD step is to "repeat" the process of adding another test. What we're going to do is add our work to git and then move to Exercise #2, which is to add the second test.
 
+// Exercise #2: New tests for sayHello will each have their own expect and it inside the describe previously created. Each function you are writing and testing should typically have its own describe
+// Step 1: The smallest possible test, now that the function exists, is to ensure that calling the function gives us a string. Inside of tests.js, add an assertion to sayHello that it "should return a string when called.". The test should look similar to expect(typeof sayHello()).toBe("string")
+// Step 2: Run all tests to make sure that the new test starts red.
+//     Step 3: Have your sayHello function return a string. The simplest code and smallest change possible is to return an empty string return "".
+//     Step 4: Now, run all the tests to ensure that the previously red test is now turned green by our impelementation.
+//     Step 5: There's nothing to refactor.
+// Step 6: Repeat (Repeat the process by moving to build the next, small test)
+// Always: Add, commit, and push your work to GitHub.
+
 
 describe("helloWorld", function() {
-    it('should be a defined function', function () {
+    it("should be a defined function", function () {
         expect(typeof helloWorld).toBe("function");
+    });
+    it("should return a string when called.", function () {
+        expect(typeof helloWorld()).toBe("string")
     });
 });
 
